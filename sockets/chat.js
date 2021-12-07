e9d44bb2fb0c16eebfdd523f1e07a012ac5c052a
Sam Tok <sarinjiodtu@gmail.com>
2021-12-06 21:32:23 -0600

    user sends messages

--- sockets/chat.js
+++ sockets/chat.js
@@ -1,6 +1,11 @@
 module.exports = (io, socket) => {
-    socket.on("new user", (username) => {
-        console.log(`${username} has joined the chat!`)
-        io.emit('new user', username)
+    socket.on('new user', (username) => {
+        console.log(`${username} has joined the chat!`);
+        io.emit('new user', username);
+    });
+
+    socket.on('new message', (data)=>{
+        console.log(`${data.sender}: ${data.message}`);
+        io.emit('new message', data);
     })
 }
